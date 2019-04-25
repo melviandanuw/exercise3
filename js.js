a2a81ce53da66d86d475da25cead4e977d3bcca2
Melvian Danu Wijaya <49687656+melviandanuw@users.noreply.github.com>
2019-04-25 13:12:02 +0700

    Filter Box
    Exercise 3

--- js.js
+++ js.js
@@ -1,19 +1,18 @@
 function filter1() {
-    var input, filter, table, tr, td, i, txtvalue;
-    input = document.getElementById('boxSearch');
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("boxSearch");
     filter = input.value.toUpperCase();
-    table = document.getElementById('table');
-    tr = table.getElementById('tr');
-
-    for (i = 0; i < tr.length; i++); {
-        td = tr[i].getElementById('td')[0];
-        if (td){
-            txtvalue = td.textContent || td.innertext;
-            if (txtvalue.toUpperCase().indexOf(filter) > -1){
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
+      }       
     }
-}
+  }
